f4bde64de14be474eb3b0859724f7b554f401e79
emilhajizade <emilhacizade44@mail.ru>
2022-09-01 21:16:05 +0400

    Task optimaized

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const damas = document.querySelectorAll(`tbody td:not(.given-number)`);
-const candidateSwitch = document.querySelector(`#candidate-switch`);//number-controls
+const candidateSwitch = document.querySelector(`#candidate-switch`);
 const numberControls = document.querySelectorAll(`.number-control`);
- //candidate-switch
-console.log(damas);
 let lastItem ;
-console.log(lastItem);
 //Damalara click eden zaman ferqlidirse silsin select classini
 damas.forEach(dama =>{
     dama.addEventListener("click",function (e) {
         dama.classList.add("selected")
         lastItem != dama ? removeClass(lastItem,"selected") : null
         lastItem = dama;
-        // dama.children[0].innerHTML = "123456";
-        // dama.children[1].innerHTML = "1";
       })
 })
-
-
 
 //contolda butona basanda butonun text ini 
 numberControls.forEach(nc=>{
@@ -27,24 +20,18 @@ numberControls.forEach(nc=>{
     }
     // document.querySelector(".selected").children[0].innerHTML = e.target.innerHTML //sil
     let candidateText = document.querySelector(".selected").children[0].innerHTML
-    console.log("candidateText secilmis olan elementin kohne deyeri",candidateText);
     let candidateArr = changeStringToArr(candidateText)
     if (!candidateArr.includes(e.target.innerHTML)) {
         candidateArr.push(e.target.innerHTML)
+        candidateArr.sort()
     }
-    
-    candidateArr.sort()
-    console.log("push olunmus",candidateArr);
+
     candidateArr = candidateArr.join("")
-    console.log("stringlesmis",candidateArr);
-
     document.querySelector(".selected").children[0].innerHTML = candidateArr
-
       })
 })
 
 candidateSwitch.addEventListener("click",function (e) {
-    console.log("input chekced ? :",e.target.checked );
     return e.target.checked 
   })
 
@@ -53,9 +40,7 @@ function removeClass(element,className) {
 }
 
 function changeStringToArr(str) {
-    
     return str.split("")
-    //sortu kanarda ver
 }
 
 var text = "salam"
